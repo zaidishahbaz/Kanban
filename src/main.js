f570fed67a55069c53c5846fc9d96da7fdc1348c
zaidishahbaz <shahbazzaidi15@gmail.com>
2019-09-24 16:42:09 +0530

    Front end done

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+// import "uikit/dist/css/uikit.min.css";
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
